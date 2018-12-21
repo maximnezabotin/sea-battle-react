fea09b4ce1d0c2f651a32879688ec8a267f924c5
Maxim Mochalov <maxim.mochalov@akvelon.com>
2018-12-21 18:52:57 +0300

    change styles

--- src/app/field/cell/cell.js
+++ src/app/field/cell/cell.js
@@ -4,14 +4,16 @@ import './cell.css'
 export default class Cell extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.state = {isActive: false};
     this.click = this.click.bind(this);
   }
 
   click() {
+    this.setState({isActive: true});
     console.log(this.props.pos);
   }
 
   render() {
-    return (<div className="cell"><div className="square" onClick={this.click}></div></div>);
+    return (<div className="cell"><div className={this.state.isActive ? 'square active' : 'square'} onClick={this.click}></div></div>);
   }
 }
